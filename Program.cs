a33da5f9a5da58f0641930b088806e914ddb5793
Fabio Pinciroli <fabio@pinciroli.xyz>
2025-07-19 16:20:48 +0200

    First upload

---
+++ Program.cs
@@ -0,0 +1,268 @@
+namespace busyLightNG
+{
+    using System;
+    using System.IO.Ports;
+    using System.Timers;
+    using busyLightNG.resources;
+    using Microsoft.Win32;
+
+    /*TODO---------------------------------------------------
+     - Logica di riconnessione periodica
+    -------------------------------------------------------*/
+
+    internal class Program
+    {
+        //Menu stuff
+        public static NotifyIcon icon;
+        public static ContextMenuStrip IconMenuStrip;
+        public static ToolStripMenuItem ExitButton;
+        public static ToolStripMenuItem InfoButton;
+        public static ToolStripMenuItem SerialButton;
+        public static ToolStripMenuItem ModeButton;
+        public static ToolStripMenuItem ModeAutoButton;
+        public static ToolStripMenuItem ModeManualOnButton;
+        public static ToolStripMenuItem ModeManualOffButton;
+        public static ToolStripMenuItem AboutButton;
+        public static System.Timers.Timer UpdateTimer;
+
+        //State
+        static bool ReconnectState = false;
+
+        //Serial
+        public static SerialPort SPort;
+
+        //Entry point
+        [STAThread]
+        static void Main()
+        {
+            //Housekeeping
+            ApplicationConfiguration.Initialize();
+            UpdateTimer = new System.Timers.Timer(1000);
+            UpdateTimer.Elapsed += UpdateStatusStub;
+            UpdateTimer.AutoReset = true;
+
+
+            //Set up systray icon and change appearance
+            icon = new NotifyIcon();
+            icon.Icon = Properties.Resources.ico_err;
+
+            //Set up context menu and items
+            IconMenuStrip = new ContextMenuStrip();
+
+            InfoButton = new ToolStripMenuItem("Info"); //Info line
+            InfoButton.Text = "BusyLightNG 1.0";
+            InfoButton.Enabled = false;
+            IconMenuStrip.Items.Add(InfoButton);
+
+            ModeAutoButton = new ToolStripMenuItem("Auto"); //Mode auto button
+            ModeAutoButton.Image = Properties.Resources.Activity;
+            ModeAutoButton.Text = "Automatic";
+            ModeAutoButton.Checked = true;
+            ModeAutoButton.Click += new System.EventHandler(ModeAutoButtonClick);
+
+            ModeManualOnButton = new ToolStripMenuItem("ManOn"); //Mode manual on button
+            ModeManualOnButton.Image = Properties.Resources.BreakpointTemporary;
+            ModeManualOnButton.Text = "Manual - On call";
+            ModeManualOnButton.Click += new System.EventHandler(ModeManOnButtonClick);
+
+            ModeManualOffButton = new ToolStripMenuItem("ManOff"); //Mode manual off button
+            ModeManualOffButton.Text = "Manual - Available";
+            ModeManualOffButton.Image = Properties.Resources.BreakpointTemporaryAvailable;
+            ModeManualOffButton.Click += new System.EventHandler(ModeManOffButtonClick);
+
+            ModeButton = new ToolStripMenuItem("Mode"); //Mode button
+            ModeButton.Text = "Mode";
+            ModeButton.Image = Properties.Resources.Settings;
+            ModeButton.DropDownItems.AddRange([ModeAutoButton, ModeManualOnButton, ModeManualOffButton]);
+            ModeButton.Click += new System.EventHandler(ModeAutoButtonClick);
+            IconMenuStrip.Items.Add(ModeButton);
+
+            SerialButton = new ToolStripMenuItem("Serial"); //Mode button
+            SerialButton.Text = "Serial port";
+            SerialButton.Image = Properties.Resources.ConnectToEnvironment;
+            UpdateSerialPorts();    //Manually update serial port listing
+            IconMenuStrip.Items.Add(SerialButton);
+
+            AboutButton = new ToolStripMenuItem("About"); //About button
+            AboutButton.Image = Properties.Resources.QuestionMark;
+            AboutButton.Text = "Exit";
+            IconMenuStrip.Items.Add(AboutButton);
+            AboutButton.Click += new System.EventHandler(AboutButtonClick);
+
+            ExitButton = new ToolStripMenuItem("Exit"); //Exit button
+            ExitButton.Image = Properties.Resources.Close;
+            ExitButton.Text = "About";
+            IconMenuStrip.Items.Add(ExitButton);
+            ExitButton.Click += new System.EventHandler(ExitButtonClick);
+
+            //Display icon
+            icon.ContextMenuStrip = IconMenuStrip;
+            icon.Visible = true;
+
+            //Set up serial port
+            SPort = new SerialPort();
+            SPort.BaudRate = 115200;
+            SPort.Parity = Parity.None;
+            SPort.DataBits = 8;
+            SPort.StopBits = StopBits.One;
+            SPort.Handshake = Handshake.None;
+
+            Application.Run();  //Spawn main thread without forms
+        }
+
+        //Menu event handlers
+        private static void ExitButtonClick(object sender, EventArgs e) //On exit, close serial port before quitting
+        {
+            SPort.Close();
+            Application.Exit();
+        } 
+        private static void ModeAutoButtonClick(object sender, EventArgs e)
+        {
+            ModeManualOnButton.Checked = false; //Change ticked line
+            ModeManualOffButton.Checked = false;
+            ModeAutoButton.Checked = true;
+            ReconnectState = false; //Show error if serial port fails
+            UpdateStatus(); //Run a status update immediately (to avoid the 1s delay)
+            UpdateTimer.Start();    //Start timer
+        }
+        private static void ModeManOnButtonClick(object sender, EventArgs e)
+        {
+            ModeManualOnButton.Checked = true;  //Change ticked line
+            ModeManualOffButton.Checked = false;
+            ModeAutoButton.Checked = false;
+            UpdateTimer.Stop(); //Stop automatic update timer
+            icon.Icon = Properties.Resources.ico_on_man;    //Change icon
+            SetColor(1);    //Change color
+        }
+        private static void ModeManOffButtonClick(object sender, EventArgs e)
+        {
+            ModeManualOnButton.Checked = false; //Change ticked line
+            ModeManualOffButton.Checked = true;
+            ModeAutoButton.Checked = false;
+            UpdateTimer.Stop(); //Stop automatic update timer
+            icon.Icon = Properties.Resources.ico_off_man;   //Change icon
+            SetColor(0);    //Change color
+        }
+        private static void SerialButtonClick(object sender, EventArgs e)
+        {
+            if(sender.GetType().GetProperty("Name").GetValue(sender) == "Update")   //If update button, manually refresh available serial ports
+            {
+                UpdateSerialPorts();
+            } else //Otherwise, try to set up Serial connection
+            {
+                try
+                {
+                    UpdateTimer.Stop(); //Stop auto update timer (if running)
+                    SPort.Close();  //Close serial port
+                    SPort.PortName = (string)sender.GetType().GetProperty("Name").GetValue(sender); //Change port assignment
+                    SPort.Open();   //Open serial port with new port
+                    ReconnectState = false; //Show error if serial port fails
+                    UpdateStatus(); //Run status update and start timer as usual
+                    UpdateTimer.Start();
+                    icon.ShowBalloonTip(5000, "Connected to BusyLight", "Connected to BusyLight on "+ (string)sender.GetType().GetProperty("Name").GetValue(sender), ToolTipIcon.Info); //Show popup
+                }
+                catch (Exception excp)  //If serial port fails to open
+                {
+                    icon.Icon = Properties.Resources.ico_err;   //Set error icon
+                    icon.ShowBalloonTip(5000, "Unable to open serial port", excp.ToString(), ToolTipIcon.Error);    //Show popup
+                }
+            }
+
+        }
+        private static void AboutButtonClick(object sender, EventArgs e)    //When about button clicked, instanciate and show about dialog
+        {
+            AboutDialog aboutDialog = new AboutDialog();
+            aboutDialog.Show();
+        }
+
+        //Status check
+        private static void UpdateStatusStub(Object source, ElapsedEventArgs e) //Stub for unused event context data
+        {
+            UpdateStatus();
+        }
+        private static void UpdateStatus()
+        {
+            try
+            {
+                RegistryKey currUser = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Default); //Open registry hive on HKEY_CURRENT_USER
+                String[] Subkeys = currUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\CapabilityAccessManager\\ConsentStore\\microphone\\NonPackaged").GetSubKeyNames();    //Get all subfolders under the magic path
+                
+                bool MicInUse = false;  //Result register for looping with state
+
+                foreach (var item in Subkeys)   //Loop across all the subfolders
+                {
+                    long a = (long) currUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\CapabilityAccessManager\\ConsentStore\\microphone\\NonPackaged\\" + item).GetValue("LastUsedTimeStop");
+                    if (a == 0) //Check if last used time is zero
+                    {
+                        MicInUse = true;    //If it is, set register
+                    }
+                }
+
+                if (MicInUse) 
+                {
+                    if(SetColor(1)) //Apply new icon only if serial port write happens succesfully
+                    {
+                        icon.Icon = Properties.Resources.ico_on;
+                    }             
+                } else
+                {
+                    if(SetColor(0))
+                    {
+                        icon.Icon = Properties.Resources.ico_off;
+                    }  
+                }
+            }
+            catch (Exception e) //If reading from the registry fails
+            {
+                    UpdateTimer.Stop();
+                    SPort.Close();  //Close serial port
+                    icon.Icon = Properties.Resources.ico_err;   //Set error icon
+                    icon.ShowBalloonTip(5000, "Runtime error", e.ToString(), ToolTipIcon.Error);    //Show popup
+
+            }
+        }
+
+        //Serial port handling
+        private static bool SetColor(int color) //0: green, 1: red
+        {
+            try
+            {
+                if (ReconnectState)  //If connection failed before
+                {
+                    SPort.Open();   //Try to reopen the serial port (or fail silently)
+                    ReconnectState = false; //Set up context for a working port
+                    icon.ShowBalloonTip(5000, "Connected to BusyLight", "Reestabilished connection with device", ToolTipIcon.Info); //Show popup
+
+                }
+
+                SPort.Write(color.ToString());
+
+                return (true);  //Return succesful execution to calling function
+            }
+            catch (Exception e)
+            {
+                if(!ReconnectState)
+                {
+                    //UpdateTimer.Stop();
+                    ReconnectState = true;  //Set context for auto-reconnect
+                    SPort.Close();
+                    icon.Icon = Properties.Resources.ico_err;
+                    icon.ShowBalloonTip(5000, "Serial port error", e.ToString(), ToolTipIcon.Error);
+                }
+                 //Return failed execution to calling function
+            }
+
+            return (false); //Execution never ends up here, but compiler is unhappy otherwise :-)
+
+        }
+        private static void UpdateSerialPorts()
+        {
+            SerialButton.DropDownItems.Clear(); //Delete everything
+            foreach (string s in SerialPort.GetPortNames()) //Bulk add serial ports
+            {
+                SerialButton.DropDownItems.Add(new ToolStripMenuItem(s, Properties.Resources.SerialPort, SerialButtonClick, s));
+            }
+            SerialButton.DropDownItems.Add(new ToolStripMenuItem("Update", Properties.Resources.UpdateListItem, SerialButtonClick, "Update"));  //Add back update button as last
+        }
+    }
+}
---
+++ resources/AboutDialog.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace busyLightNG.resources
+{
+    partial class AboutDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel = new TableLayoutPanel();
+            logoPictureBox = new PictureBox();
+            labelProductName = new Label();
+            labelVersion = new Label();
+            labelCopyright = new Label();
+            linkLabel1 = new LinkLabel();
+            tableLayoutPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)logoPictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            tableLayoutPanel.ColumnCount = 2;
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33F));
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 67F));
+            tableLayoutPanel.Controls.Add(logoPictureBox, 0, 0);
+            tableLayoutPanel.Controls.Add(labelProductName, 1, 0);
+            tableLayoutPanel.Controls.Add(labelVersion, 1, 1);
+            tableLayoutPanel.Controls.Add(labelCopyright, 1, 2);
+            tableLayoutPanel.Controls.Add(linkLabel1, 1, 3);
+            tableLayoutPanel.Dock = DockStyle.Fill;
+            tableLayoutPanel.Location = new Point(10, 10);
+            tableLayoutPanel.Margin = new Padding(4, 3, 4, 3);
+            tableLayoutPanel.Name = "tableLayoutPanel";
+            tableLayoutPanel.RowCount = 5;
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 29.9674263F));
+            tableLayoutPanel.Size = new Size(487, 163);
+            tableLayoutPanel.TabIndex = 0;
+            // 
+            // logoPictureBox
+            // 
+            logoPictureBox.Dock = DockStyle.Fill;
+            logoPictureBox.Image = Properties.Resources.logo_pinciroli__monotipo;
+            logoPictureBox.Location = new Point(4, 3);
+            logoPictureBox.Margin = new Padding(4, 3, 4, 3);
+            logoPictureBox.Name = "logoPictureBox";
+            tableLayoutPanel.SetRowSpan(logoPictureBox, 5);
+            logoPictureBox.Size = new Size(152, 157);
+            logoPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            logoPictureBox.TabIndex = 12;
+            logoPictureBox.TabStop = false;
+            // 
+            // labelProductName
+            // 
+            labelProductName.Dock = DockStyle.Fill;
+            labelProductName.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            labelProductName.Location = new Point(167, 0);
+            labelProductName.Margin = new Padding(7, 0, 4, 0);
+            labelProductName.MaximumSize = new Size(0, 20);
+            labelProductName.Name = "labelProductName";
+            labelProductName.Size = new Size(316, 20);
+            labelProductName.TabIndex = 19;
+            labelProductName.Text = "BusyLightNG controller";
+            labelProductName.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // labelVersion
+            // 
+            labelVersion.Dock = DockStyle.Fill;
+            labelVersion.Location = new Point(167, 23);
+            labelVersion.Margin = new Padding(7, 0, 4, 0);
+            labelVersion.MaximumSize = new Size(0, 20);
+            labelVersion.Name = "labelVersion";
+            labelVersion.Size = new Size(316, 20);
+            labelVersion.TabIndex = 0;
+            labelVersion.Text = "v1.0";
+            labelVersion.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // labelCopyright
+            // 
+            labelCopyright.CausesValidation = false;
+            labelCopyright.Dock = DockStyle.Fill;
+            labelCopyright.Location = new Point(167, 46);
+            labelCopyright.Margin = new Padding(7, 0, 4, 0);
+            labelCopyright.MaximumSize = new Size(0, 20);
+            labelCopyright.Name = "labelCopyright";
+            labelCopyright.Size = new Size(316, 20);
+            labelCopyright.TabIndex = 21;
+            labelCopyright.Text = "Distributed under GNU Affero General Public License v3.0";
+            labelCopyright.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Dock = DockStyle.Fill;
+            linkLabel1.LinkBehavior = LinkBehavior.HoverUnderline;
+            linkLabel1.Location = new Point(163, 69);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(321, 23);
+            linkLabel1.TabIndex = 24;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Pinciroli.XYZ | Source code";
+            linkLabel1.TextAlign = ContentAlignment.MiddleLeft;
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // AboutDialog
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(507, 183);
+            Controls.Add(tableLayoutPanel);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Margin = new Padding(4, 3, 4, 3);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "AboutDialog";
+            Padding = new Padding(10);
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "About";
+            tableLayoutPanel.ResumeLayout(false);
+            tableLayoutPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)logoPictureBox).EndInit();
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.PictureBox logoPictureBox;
+        private Label labelProductName;
+        private Label labelVersion;
+        private Label labelCopyright;
+        private LinkLabel linkLabel1;
+    }
+}
